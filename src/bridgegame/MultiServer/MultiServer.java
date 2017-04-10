1b9f0828a8d72830b8afbf9a7622ba654f134570
Box1570 <box1570@gmail.com>
2017-04-10 17:43:20 +0200

    Struttura MultiServer
    d.i.o

---
+++ src/bridgegame/MultiServer/MultiServer.java
@@ -0,0 +1,24 @@
+package bridgegame.MultiServer;
+
+import java.net.*; 
+import java.io.*; 
+ 
+public class MultiServer { 
+   
+    public static void main(String[] args) throws IOException { 
+       
+        ServerSocket serverSocket = null; 
+        boolean listening = true; 
+ 
+        try { 
+            serverSocket = new ServerSocket(4444); 
+        } catch (IOException e) { 
+            System.err.println("Could not listen on port: 4444."); 
+        } 
+ 
+        while (listening) 
+        new MultiServerThread(serverSocket.accept()).start(); 
+ 
+        serverSocket.close(); 
+    } 
+}
---
+++ src/bridgegame/MultiServer/MultiServerThread.java
@@ -0,0 +1,40 @@
+package bridgegame.MultiServer;
+
+import java.net.*; 
+import java.io.*; 
+
+public class MultiServerThread extends Thread { 
+	private Socket socket = null; 
+	public MultiServerThread(Socket socket) { 
+		super("MultiServerThread");//TODO WTF? 
+		this.socket = socket; 
+	} 
+
+	public void run() { 
+
+		try { 
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true); 
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); 
+
+			String inputLine, outputLine; 
+
+			/*TODO thread creato, invio del grafo e inizio trasmissione tabella posizioni aggiornata 
+      Ricevo spostamenti dal client, una volta ottenuti tutti gli spostamenti 
+			 */
+
+			out.println(/*outputLine*/); 
+
+			inputLine = in.readLine();
+			out.println(/*outputLine*/); 
+
+
+
+			out.close(); 
+			in.close(); 
+			socket.close(); 
+
+		} catch (IOException e) { 
+			e.printStackTrace(); 
+		} 
+	} 
+}
