c5902c52adec8ed93ecaee03081aebdc53c1d784
NSilv <n.silvestrin98@gmail.com>
2017-04-10 17:54:11 +0200

    Cambiato nome package

--- src/bridgegame/MultiServer/MultiServer.java
+++ src/bridgegame/MultiServer/MultiServer.java
@@ -1,4 +1,4 @@
-package bridgegame.MultiServer;
+package bridgegame.multiserver;
 
 import java.net.*; 
 import java.io.*; 
@@ -17,7 +17,7 @@ public class MultiServer {
         } 
  
         while (listening) 
-        new MultiServerThread(serverSocket.accept()).start(); 
+        	new MultiServerThread(serverSocket.accept()).start(); 
  
         serverSocket.close(); 
     } 
